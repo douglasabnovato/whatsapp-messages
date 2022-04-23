7a3b575eb982af9a7542c3ae084bf99739c4c033
Douglas A B Novato <douglasabnovato@gmail.com>
2022-04-23 15:14:47 -0300

    validar preenchimento, não preenchimento, cancelar

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ function hello (elementMessage){
     let messageText = elementMessage.firstElementChild.innerText
     let numberDestination = window.prompt("Para qual número deseja enviar a mensagem? DD 9XXXX-XXXX");
 
-    if (numberDestination == null || numberDestination == "") {
-        alert("Preencher o número que deseja enviar a mensagem via Whatsapp!");
+    if (numberDestination == "") {
+        alert("Preencher com o número que deseja enviar a mensagem via Whatsapp!");
+    } else if (numberDestination == null) {
+        alert("Volte sempre amigo!");
     } else {
         let toWhatsapp = `https://api.whatsapp.com/send?phone=55${numberDestination}&text=${messageText}`
         window.open(toWhatsapp);
