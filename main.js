b8c3350b1bf71f8c1a773d97595b8a6f49544609
Douglas A B Novato <douglasabnovato@gmail.com>
2022-04-23 15:10:30 -0300

    hospedado em githup pages

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ message.addEventListener("click", hello)
 
 function hello (elementMessage){
     let messageText = elementMessage.firstElementChild.innerText
-    let numberDestination = window.prompt("Para qual número deseja enviar a mensagem? DD 9XXXX-XXXX", "Insira um número válido!");
+    let numberDestination = window.prompt("Para qual número deseja enviar a mensagem? DD 9XXXX-XXXX");
 
     if (numberDestination == null || numberDestination == "") {
         alert("Preencher o número que deseja enviar a mensagem via Whatsapp!");
